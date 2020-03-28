8ff1189ba606543fb2dad711a82ef82792d4475d
Piyush Subedi <16piyush02@gmail.com>
2020-03-27 20:48:01 -0400

    Working code

--- feedback_retriever.js
+++ feedback_retriever.js
@@ -6,13 +6,14 @@ import { readFileSync, writeFileSync } from 'fs';
 // const URL_ELC_HOME = 'https://cas2.uga.edu/cas/login?service=https://ssomanager-prod.uga.edu:443/ssomanager/c/SSB';
 const URL_ELC_HOME = 'https://sso.uga.edu/cas/login?service=https%3a%2f%2fuga.view.usg.edu%2fd2l%2fcustom%2fcas';
 
-const URL_COURSE_HOME = 'https://uga.view.usg.edu/d2l/home/';
+const URL_GRADES_VIEW = 'https://uga.view.usg.edu/d2l/lms/grades/admin/enter/user_list_view.d2l?ou=';
 
+const OUTPUT_FILE = 'output.json';
 const PASSCODES_FILENAME = './config/passcodes.json';
 
-const DEVELOPMENT_MODE = true;
-
 const COURSE_ID = '1955242';
+
+const DEVELOPMENT_MODE = false;
 
 class FeedbackRetriever {
 
@@ -31,6 +32,15 @@ class FeedbackRetriever {
         return new Promise(resolve => setTimeout(resolve, time));
     }
 
+    async filter(arr, callback) {
+        const fail = Symbol()
+        return (await Promise.all(arr.map(async item => (await callback(item)) ? item : fail))).filter(i=>i!==fail);
+    }
+
+    async click(element) {
+        await this.driver.executeScript("arguments[0].click();", element);
+    }
+
     async loginToELC() {
 
         console.log("Logging in to your ELC account");
@@ -40,17 +50,19 @@ class FeedbackRetriever {
         await element.sendKeys(this.username);
         let element_1 = await this.driver.findElement({ name: 'password' });
         await element_1.sendKeys(this.password);
-        await this.click({ name: 'submit' });
+        let submitBtn = await this.driver.findElement({ name: "submit" });
+        await this.click(submitBtn);
 
         // duo frame begins
         await this.driver.switchTo().frame('duo_iframe');
-        await this.click({ id: 'passcode' });
+        let passcodeElem = await this.driver.findElement({ id : "passcode"});
+        await this.click(passcodeElem);
         let element_3 = await this.driver.findElement({ name: 'passcode' });
         await element_3.sendKeys(this.passcode);
-        await this.click({ id: 'passcode' });
+        passcodeElem = await this.driver.findElement({ id : "passcode"});
+        await this.click(passcodeElem);
         await this.driver.switchTo().defaultContent();
     }
-
 
     async loadCourseHome() {
 
@@ -81,99 +93,122 @@ class FeedbackRetriever {
 
     async loadGrades() {
 
+        this.driver.get(URL_GRADES_VIEW + this.courseId);
+
         // document.getElementsByTagName("d2l-navigation")[0].getElementsByTagName("d2l-navigation-main-footer")[0].childNodes[0].childNodes[0]
 
-        let navElements = await this.driver.findElements({ tagName: "d2l-navigation" })[0];
+        // let navElements = await this.driver.findElements({ tagName: "d2l-navigation" });
 
-        let navFooter = await navElements.findElements({ tagName: "d2l-navigation-main-footer" })[0];
+        // let navFooters = await navElements[0].findElements({ tagName: "d2l-navigation-main-footer" });
 
-        let navItems = navFooter.findElement(By.xpath(".//div//div"));
+        // let navItems = await navFooters[0].findElements(By.xpath(".//div//div//div"));
 
-        let gradeNavItem = navItems.map(async navItem => {
-            let navItemLink = navItem.findElement(By.xpath(".//a"));
-            return navItemLink.getText() === "Grades";
-        });
+        // let gradeNavItems = navItems.map(async navItem => {
+        //     let navItemLinks = await navItem.findElements({ tagName: "a" });
 
-        this.click(gradeNavItem);
+        //     if (navItemLinks[0]) {
+        //         let linkText = await navItemLinks[0].getText();
+        //         return linkText === "Grades";
+        //     }
+
+        //     return false;
+        // });
+
+        // let results = await Promise.all(gradeNavItems);
+
+        // await navItems[results.indexOf(true)].click();
     }
 
+    async loadFeedbackView() {
 
-    async loadCourses() {
-        await this.click({ id: 'bmenu--P_StuMainMnu___UID1' });
-        await this.waitWhileThePageLoadsUp(2000);
-        await this.click({ id: 'bmenu--P_RegMnu___UID0' });
-        await this.waitWhileThePageLoadsUp(2000);
-        await this.click({ id: 'contentItem12' });
-        await this.selectFromList({ name: 'p_term' }, this.term);
-        await this.click({ id: 'id____UID6' });
-        await this.click({ id: 'id____UID5' });
-        await this.selectFromList({ id: 'subj_id' }, this.subject);
-        await this.selectFromList({ id: 'levl_id' }, this.courseLevel);
-        await this.click({ id: 'id____UID4' });
+        let userTbl = await this.driver.findElement({ id : "z_bk" });
+
+        let firstUser = await userTbl.findElement(By.xpath(".//tbody//tr[3]//a[3]"));
+
+        await firstUser.click();
     }
 
-    async lookIfCoursesAreOpenAndRegister() {
+    async collectUserFeedbacks(result) {
 
-        let element = await this.driver.findElement({ className: 'datadisplaytable' });
-        let element_1 = await element.findElement({ tagName: 'tbody' });
-        let trs = await element_1.findElements({ tagName: 'tr' });
+        // get username
+        let headings = await this.driver.findElements({ className : "vui-heading-1"});
 
-        const promises = trs.map(async tr => {
-            const tds = await tr.findElements({ tagName: 'td' });
+        let username = await headings[0].getText();
 
-            if (tds && tds[7]) {
-                const tdText = await tds[7].getText();
-                if (this.desiredCourses.indexOf(tdText) !== -1) {
-                    const selectionControls = await tds[0].findElements({ name: 'sel_crn' });
-                    if (selectionControls && selectionControls.length && selectionControls.length === 1) {
-                        console.log(tdText + " is open. Selecting it for registration");
-                        await this.driver.executeScript("arguments[0].click();", selectionControls[0]);
+        // expand all comment sections
+        let commentsTogglers = await this.driver.findElements({ className: "di_l d2l-link d2l-link-inline" });
 
-                        return true;
-                    }
-                }
-            }
-
-            return false;
+        commentsTogglers = await this.filter(commentsTogglers,async tg => {
+            let text = await tg.getText();
+            return text === 'Show Comments';
         });
 
-        let results = await Promise.all(promises);
-        results = results.filter((result) => { return result === true; });
-        if (results.length > 0) {
-            // this means we were able to select one of the desired courses
-            console.log(results.length + " of the desired courses were available and have been selected for registration. Saving the registration");
-            if(!DEVELOPMENT_MODE) {
-                await this.click({ id: "id____UID4" });
-            }
-        } else {
-            console.log("No courses were registered possibly due to unavailability");
+        for (let i=0; i < commentsTogglers.length; i++) {
+            await this.driver.executeScript("arguments[0].click()", commentsTogglers[i]);
         }
 
+        // retrieve the feedbacks
+        let commentsDivs = await this.driver.findElements({ className: "d2l-htmlblock d2l-htmlblock-deferred d2l-htmlblock-untrusted" });
+
+        // start from 3rd div and skip one
+        let feedbacks = [];
+        for (let i=2; i < commentsDivs.length; i += 2) {
+            let feedback = await commentsDivs[i].getText();
+            feedbacks.push(feedback);
+        }
+
+        result[username] = feedbacks;
+
+        // console.log (result);
     }
 
-    async click(elem) {
-        let element = await this.driver.findElement(elem);
+    async nextUser() {
 
-        // using executeScript instead of click() because click() has an issue when the element being clicked is not in the view
-        //await element.click();
-        await this.driver.executeScript("arguments[0].click();", element);
+        let nextBtnCls = await this.driver.findElements({ className: "d2l-iterator-button d2l-iterator-button-next d2l-iterator-button-notext"});
 
+        let isDisabled = await nextBtnCls[0].getAttribute("aria-disabled");
+
+        if (!isDisabled) {
+            await nextBtnCls[0].click();
+        }
+
+        return !isDisabled;
     }
-
-    async selectFromList(selectElement, optionToSelect) {
-        let element = await this.driver.findElement(selectElement);
-
-        await element.sendKeys(optionToSelect);
-    }
-
 
     async start() {
-        await this.loginToELC();
-        await this.waitWhileThePageLoadsUp(5000);
-        await this.loadCourseHome();
-        await this.loadGrades();
-        // await this.lookIfCoursesAreOpenAndRegister();
-        await this.driver.close();
+
+        try {
+
+            if (DEVELOPMENT_MODE) {
+                await this.driver.get("file:///C:/Users/16piy/Documents/TestScripts/uga_elc_feedback_retriever/dw_pages/first_user_view.html");
+                await this.waitWhileThePageLoadsUp(1000);
+            } else {
+                await this.loginToELC();
+                await this.waitWhileThePageLoadsUp(5000);
+                await this.loadGrades();
+                await this.waitWhileThePageLoadsUp(5000);
+                await this.loadFeedbackView();
+                await this.waitWhileThePageLoadsUp(5000);
+            }
+            
+            let hasNext = true;
+            
+            let result = {}
+            
+            while (hasNext) {
+                await this.collectUserFeedbacks(result);
+                hasNext = await this.nextUser();
+            }
+
+            // console.log( JSON.stringify (result));
+
+            writeFileSync(OUTPUT_FILE, JSON.stringify(result), 'utf8');
+            
+        } catch (error) {
+            console.log (error);
+        } finally {
+            await this.driver.close();
+        }
 
     }
 
@@ -195,8 +230,6 @@ function setPassCodes(passCodes) {
 
 async function startJob() {
 
-    let job = undefined;
-
     let passCodes = getPassCodes();
 
     if(passCodes && passCodes.length > 0) {
@@ -204,27 +237,19 @@ async function startJob() {
 
         let automater = new FeedbackRetriever(config.MY_USERNAME, config.MY_PASSWORD, passCodes[0], COURSE_ID);
         
-        try {
-            await automater.start();
-        } catch(error) {
-            console.log("There was an error. " + JSON.stringify(error));
-            // consuming error such that the job can retry; maybe one of the passcodes was wrong 
-        }
+        await automater.start();
 
         // remove the used passcode
         setPassCodes(passCodes.slice(1));
-        
-        if(!job) {
-            job = setInterval(startJob, config.INTERVAL_IN_MINS * 60 * 1000);
-        }
 
     } else {
         console.log("OUT OF PASSCODES. CANNOT PROCEED. FILL THE passcodes.json WITH VALID PASSCODES TO PROCEED");
-        if(job) {
-            clearInterval(job);
-        }
     }
 
 }
 
-startJob();
+try {
+    startJob();
+} catch (error) {
+    console.log("MAIN ERROR " + JSON.stringify(error));
+}
